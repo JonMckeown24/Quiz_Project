220761708320f2f160757c6b1d49f324c33628c5
JonMcKeown24 <jonathan-mckeown@live.nmit.ac.nz>
2024-03-13 12:02:23 +1300

    Added questions

--- src/quiz.js
+++ src/quiz.js
@@ -1,1 +1,22 @@
 // Install node.js (how you can run the program and test the program)
+
+//Install Readline later
+
+const question = [
+    {
+        question : 'what is 2 + 2',
+        options : ['2', '3', '4', '5'],
+        answer : '4'
+    },
+    {
+        question : 'What is the capital of New Zealand',
+        options : ['Auckland', 'Wellington', 'Nelson', 'Christchurch'],
+        answer : 'Wellington'
+    }
+];
+// Add more questions here
+
+// Declare quiz function 
+function quiz() {
+    
+}
